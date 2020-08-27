e0d010b32617fe49f1df695259c0a2259fac6d4f
fanquake <fanquake@gmail.com>
2020-08-27 16:36:24 +0800

    build: remove freetype_back_compat.patch for qt 5.15
    By the time we ship a release with Qt 5.15, we'll certainly no-longer be
    supporting Ubuntu 14.04 and Ubuntu 16.04 ships with FreeType 2.6.1,
    which is new enough that using the symbol is no-longer an issue.
    
    The renaming of FT_Get_X11_Font_Format() happened in FreeType 2.6

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -10,7 +10,7 @@ $(package)_build_subdir=qtbase
 $(package)_qt_libs=corelib network widgets gui plugins testlib
 $(package)_patches=fix_qt_pkgconfig.patch mac-qmake.conf fix_no_printer.patch no-xlib.patch
 $(package)_patches+= fix_android_jni_static.patch dont_hardcode_pwd.patch
-$(package)_patches+= freetype_back_compat.patch drop_lrelease_dependency.patch fix_mingw_cross_compile.patch
+$(package)_patches+= drop_lrelease_dependency.patch fix_mingw_cross_compile.patch
 
 # Update OSX_QT_TRANSLATIONS when this is updated
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
@@ -215,7 +215,6 @@ endef
 # 8. Adjust a regex in toolchain.prf, to accomodate Guix's usage of
 # CROSS_LIBRARY_PATH. See #15277.
 define $(package)_preprocess_cmds
-  patch -p1 -i $($(package)_patch_dir)/freetype_back_compat.patch && \
   patch -p1 -i $($(package)_patch_dir)/drop_lrelease_dependency.patch && \
   patch -p1 -i $($(package)_patch_dir)/dont_hardcode_pwd.patch && \
   patch -p1 -i $($(package)_patch_dir)/fix_qt_pkgconfig.patch && \
