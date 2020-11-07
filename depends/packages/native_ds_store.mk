e9d06da0d24012f015cbd12529908ee8b7f3e949
fanquake <fanquake@gmail.com>
2020-11-07 16:58:21 +0800

    depends: native_dsstore 67e238d3dc8427891d2c1c7f1d1fa6194c44a5a6
    Also removes native_biplist dependency.

--- depends/packages/native_ds_store.mk
+++ depends/packages/native_ds_store.mk
@@ -1,10 +1,9 @@
 package=native_ds_store
-$(package)_version=1.1.2
+$(package)_version=67e238d3dc8427891d2c1c7f1d1fa6194c44a5a6
 $(package)_download_path=https://github.com/al45tair/ds_store/archive/
-$(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=3b3ecb7bf0a5157f5b6010bc3af7c141fb0ad3527084e63336220d22744bc20c
+$(package)_file_name=$($(package)_version).tar.gz
+$(package)_sha256_hash=ba30d5126fd7ff50e6b1aa3d8a57ffb28c6000dd5ad0ca526d10d4fadb480a41
 $(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
-$(package)_dependencies=native_biplist
 
 define $(package)_build_cmds
     python3 setup.py build
