4469256f711b5b16e792599983b712635b030ef9
Sjors Provoost <sjors@sprovoost.nl>
2021-03-18 14:17:39 +0100

    Move external signer out of wallet module
    This commit moves the ExternalSigner class and RPC methods out of the wallet module.
    
    The enumeratesigners RPC can be used without a wallet since #21417.
    With additional modifications external signers could be used without a wallet in general, e.g. via signrawtransaction.
    
    The signerdisplayaddress RPC is ranamed to walletdisplayaddress because it requires wallet context.
    A future displayaddress RPC call without wallet context could take a descriptor argument.
    
    This commit fixes a rpc_help.py failure when configured with --disable-wallet.

--- src/external_signer.h
+++ src/external_signer.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_WALLET_EXTERNAL_SIGNER_H
-#define BITCOIN_WALLET_EXTERNAL_SIGNER_H
+#ifndef BITCOIN_EXTERNAL_SIGNER_H
+#define BITCOIN_EXTERNAL_SIGNER_H
 
 #include <stdexcept>
 #include <string>
@@ -70,4 +70,4 @@ public:
 #endif
 };
 
-#endif // BITCOIN_WALLET_EXTERNAL_SIGNER_H
+#endif // BITCOIN_EXTERNAL_SIGNER_H
