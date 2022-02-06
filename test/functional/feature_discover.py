b41f962f349ab3d3f8a4d8e761ac5592120f8576
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-06 11:55:59 -0500

    test: feature_discovery.py default case impl

--- test/functional/feature_discover.py
+++ test/functional/feature_discover.py
@@ -2,8 +2,8 @@
 # Copyright (c) 2022 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-"""Test -discover command."""
 import socket
+from socket import inet_pton, AF_INET, AF_INET6
 
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import (
@@ -11,8 +11,23 @@ from test_framework.util import (
     assert_greater_than,
 )
 
+def assertAddressStringTests():
+    assert(inet_pton(AF_INET, '255.255.255.255'))
+    assert(inet_pton(AF_INET, "255.255.255.255"))
+    assert(inet_pton(AF_INET6, '::FFFF:192.1.1.1'))
+    assert(inet_pton(AF_INET6, "::FFFF:192.1.1.1"))
+
+def testAddressDict(self,local_addrs):
+    if (socket.AF_UNIX):
+        for address in local_addrs:
+            score, score_int = address.popitem()
+            port, port_int = address.popitem()
+            address, address_string = address.popitem()
+            self.log.info("Test if the IPv4/6 addresses are valid %s",address_string)
+            socket.inet_pton(socket.AF_INET6, address_string)
 
 class DiscoverTest(BitcoinTestFramework):
+    """Test -discover command."""
     def set_test_params(self):
         self.setup_clean_chain = True
         self.num_nodes = 1
@@ -21,16 +36,37 @@ class DiscoverTest(BitcoinTestFramework):
     def run_test(self):
         self.log.info("Test node (-discover=0) has 0 local addresses")
         assert_equal(self.nodes[0].getnetworkinfo()["localaddresses"], [])
+        assertAddressStringTests()
 
-        self.log.info("Restart node with -discover=1 and check there are local addresses")
+        # default cases
+        self.log.info("Restart node with -listen -discover")
+        self.log.info("and check there are local addresses")
+        self.restart_node(0, ["-listen", "-discover"])
+        local_addrs = list(self.nodes[0].getnetworkinfo()["localaddresses"])
+        assert_greater_than(len(local_addrs), 0)
+        testAddressDict(self,local_addrs)
+
+        # additional cases
+        self.log.info("Restart node with -listen=1 -discover")
+        self.log.info("and check there are local addresses")
+        self.restart_node(0, ["-listen=1", "-discover"])
+        local_addrs = list(self.nodes[0].getnetworkinfo()["localaddresses"])
+        assert_greater_than(len(local_addrs), 0)
+        testAddressDict(self,local_addrs)
+
+        self.log.info("Restart node with -listen -discover=1")
+        self.log.info("and check there are local addresses")
         self.restart_node(0, ["-listen", "-discover=1"])
-        local_addresses = list(self.nodes[0].getnetworkinfo()["localaddresses"])
-        assert_greater_than(len(local_addresses), 0)
+        local_addrs = list(self.nodes[0].getnetworkinfo()["localaddresses"])
+        assert_greater_than(len(local_addrs), 0)
+        testAddressDict(self,local_addrs)
 
-        self.log.info("Test if the addresses are valid")
-        for address in local_addresses:
-            socket.inet_pton(socket.AF_INET6, address["address"])
-
+        self.log.info("Restart node with -listen=1 -discover=1")
+        self.log.info("and check there are local addresses")
+        self.restart_node(0, ["-listen=1", "-discover=1"])
+        local_addrs = list(self.nodes[0].getnetworkinfo()["localaddresses"])
+        assert_greater_than(len(local_addrs), 0)
+        testAddressDict(self,local_addrs)
 
 if __name__ == '__main__':
     DiscoverTest().main()
