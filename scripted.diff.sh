c2af43300b67503656ee0bc3c78eca59551fd982
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-13 19:03:46 -0400

    scripted.diff.sh

--- scripted.diff.sh
+++ scripted.diff.sh
@@ -3,6 +3,10 @@
 rm -rf ~/Library/Application\ Support/Bitcoin/signet/blocks
 rm -rf  src/qt/locale/**.qm
 rm -rf  src/qt/forms/*.h
+rm -rf  src/qt/moc_modaloverlay.cpp
+rm -rf  dist/Bitcoin-Qt.app
+rm -rf  Bitcoin-Qt.app
+
 
 if ! hash brew 2>/dev/null; then
 	eval "$(/usr/local/bin/brew shellenv)" || echo
@@ -21,13 +25,19 @@ do
 	$SED  -i -e 's/Syncing with network…/Syncing with notwork/' $i
 done
 }
+#src/qt/forms/receiverequestdialog.ui
 array=$(git grep -l "Unknown…" src/qt/modal*.cpp) && doit
-array=$(git grep -l "…" src/qt/modal*.cpp) && doit
-array=$(git grep -l "…" src/qt/*) && doit
+#array=$(git grep -l "…" src/qt/modal*.cpp) && doit
+#array=$(git grep -l "…" src/qt/*) && doit
 array=$(git grep -l "Syncing Headers (%1, %2%)" src/qt/*) && doit
 array=$(git grep -l "Syncing Headers (%1%)" src/qt/*) && doit
 array=$(git grep -l "Syncing with network…" src/qt/*) && doit
 
+git checkout src/qt/forms/intro.ui
+git checkout src/qt/bitcoin.cpp
 git checkout src/qt/bitcoingui.cpp
 git checkout src/qt/bitcoinstrings.cpp
-make && open Bitcoin-Qt.app
+
+git diff
+make
+#open Bitcoin-Qt.app
