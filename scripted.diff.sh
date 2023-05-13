120020f87c7439e9e49ce4dfa1efb635c696a927
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-13 19:03:46 -0400

    scripted.diff.sh

--- scripted.diff.sh
+++ scripted.diff.sh
@@ -3,6 +3,10 @@
 rm -rf ~/Library/Application\ Support/Bitcoin/signet/blocks
 rm -rf  src/qt/locale/**.qm
 rm -rf  src/qt/forms/*.h
+rm -rf  src/qt/moc_modaloverlay.cpp
+rm -rf  dist/Bitcoin-Qt.app
+rm -rf  Bitcoin-Qt.app
+
 
 if ! hash brew 2>/dev/null; then
 	eval "$(/usr/local/bin/brew shellenv)" || echo
@@ -14,20 +18,27 @@ doit(){
 for i in "${array[@]}"
 do
 	echo "$i"
-	$SED  -i -e 's/Unknown…//' $i
-	$SED  -i -e 's/…//' $i
-	$SED  -i -e 's/Syncing Headers (%1, %2%)…/Syncing Headers (%1, %2%)/' $i
-	$SED  -i -e 's/Syncing Headers (%1%)…/Syncing Headers (%1%)/' $i
-	$SED  -i -e 's/Syncing with network…/Syncing with notwork/' $i
+	$SED  -i -e 's/$j//' $i
 done
 }
-array=$(git grep -l "Unknown…" src/qt/modal*.cpp) && doit
-array=$(git grep -l "…" src/qt/modal*.cpp) && doit
-array=$(git grep -l "…" src/qt/*) && doit
-array=$(git grep -l "Syncing Headers (%1, %2%)" src/qt/*) && doit
-array=$(git grep -l "Syncing Headers (%1%)" src/qt/*) && doit
-array=$(git grep -l "Syncing with network…" src/qt/*) && doit
 
+#…
+#src/qt/forms/receiverequestdialog.ui
+#array=$(git grep -l "…" src/qt/modal*.cpp) && doit
+#array=$(git grep -l "…" src/qt/*) && doit
+
+strings=("Unknown…" "Syncing Headers (%1, %2%)…" "Syncing Headers (%1%)…" "Syncing with network…" "&amp;Options…")
+
+for j in "${string[@]}"
+do
+array=$(git grep -l $j src/qt/modal*.cpp) && doit
+done
+
+git checkout src/qt/forms/intro.ui
+git checkout src/qt/bitcoin.cpp
 git checkout src/qt/bitcoingui.cpp
 git checkout src/qt/bitcoinstrings.cpp
-make && open Bitcoin-Qt.app
+
+git diff
+make deploy
+open ./dist/Bitcoin-Qt.app
