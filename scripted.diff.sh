7004e8f9fb76957ecfc0acf0ed7d8ffee60e86af
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-13 01:04:26 -0400

    scripted.diff.sh

--- scripted.diff.sh
+++ scripted.diff.sh
@@ -10,11 +10,12 @@ do
 	gsed  -i -e 's/Syncing Headers (%1, %2%)…/Syncing Headers (%1, %2%)/' $i
 	gsed  -i -e 's/Syncing Headers (%1%)…/Syncing Headers (%1%)/' $i
 	gsed  -i -e 's/Syncing with network…/Syncing with notwork/' $i
-	gsed  -i -e 's/Unknown…/Unknown/' $i
+	gsed  -i -e 's/Unknown…//' $i
+	gsed  -i -e 's/Unknown //' $i
 done
 }
 array=$(git grep -l "Syncing Headers (%1, %2%)" src/*) && doit
 array=$(git grep -l "Syncing Headers (%1%)" src/*) && doit
 array=$(git grep -l "Syncing with network…" src/*) && doit
 array=$(git grep -l "Unknown…" src/*) && doit
-
+array=$(git grep -l "Unknown " src/*) && doit
