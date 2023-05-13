1be3a41fcb23fa65c2268d33522caf8cbf54fea9
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-13 01:11:19 -0400

    scripted.diff.sh

--- scripted.diff.sh
+++ scripted.diff.sh
@@ -7,15 +7,15 @@ doit(){
 for i in "${array[@]}"
 do
 	echo "$i"
-	gsed  -i -e 's/Syncing Headers (%1, %2%)…/Syncing Headers (%1, %2%)/' $i
-	gsed  -i -e 's/Syncing Headers (%1%)…/Syncing Headers (%1%)/' $i
-	gsed  -i -e 's/Syncing with network…/Syncing with notwork/' $i
 	gsed  -i -e 's/Unknown…//' $i
-	gsed  -i -e 's/Unknown //' $i
+	gsed  -i -e 's/…//' $i
+	#gsed  -i -e 's/Syncing Headers (%1, %2%)…/Syncing Headers (%1, %2%)/' $i
+	#gsed  -i -e 's/Syncing Headers (%1%)…/Syncing Headers (%1%)/' $i
+	#gsed  -i -e 's/Syncing with network…/Syncing with notwork/' $i
 done
 }
-array=$(git grep -l "Syncing Headers (%1, %2%)" src/*) && doit
-array=$(git grep -l "Syncing Headers (%1%)" src/*) && doit
-array=$(git grep -l "Syncing with network…" src/*) && doit
-array=$(git grep -l "Unknown…" src/*) && doit
-array=$(git grep -l "Unknown " src/qt) && doit
+array=$(git grep -l "Unknown…" src/qt/*) && doit
+array=$(git grep -l "…" src/qt/*) && doit
+#array=$(git grep -l "Syncing Headers (%1, %2%)" src/*) && doit
+#array=$(git grep -l "Syncing Headers (%1%)" src/*) && doit
+#array=$(git grep -l "Syncing with network…" src/*) && doit
